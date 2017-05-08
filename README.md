d28a476796df37fd4b7435ccbc693e8cb02abfd4
Benjamin Henrion <zoobab@gmail.com>
2017-05-08 17:26:41 +0200

    add some instructions to the README.md

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 About
 =====
 
-This image builds a JTAG Versaloon firmware for the STM32 Bluepill board.
+JTAG Versaloon firmware for the STM32 Bluepill board.
 
+Build
+=====
 
+If you have docker, you can just run ```./build.sh```, it will build the firmware for you.
+
+Binaries
+========
+
+Put binaries here.
--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ WORKDIR="$PWD/release/firmware/STM32F103C8_BluePill"
 LOGFILE="$WORKDIR/build.log"
 rm -f $LOGFILE
 exec &> >(tee -a "$LOGFILE")
-docker build --no-cache -t versaloon:latest .
+docker build -t versaloon:latest .
 docker run -v $WORKDIR:/mnt versaloon:latest cp -v /home/versaloon/code/dongle/firmware/Projects/Versaloon/GCC/*.{hex,elf} /mnt
