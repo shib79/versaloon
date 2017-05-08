f40289bea7d3afad83eba775072d2d450cea735f
Benjamin Henrion <zoobab@gmail.com>
2017-05-08 17:11:07 +0200

    add a README.md

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-LOGFILE="release/firmware/STM32F103C8_BluePill/build.log"
+WORKDIR="$PWD/release/firmware/STM32F103C8_BluePill"
+LOGFILE="$WORKDIR/build.log"
 rm -f $LOGFILE
 exec &> >(tee -a "$LOGFILE")
 docker build --no-cache -t versaloon:latest .
-#docker run -v $PWD/release/firmware/STM32F103C8_BluePill:/mnt versaloon:latest "cd "
+docker run -v $WORKDIR:/mnt versaloon:latest cp -v /home/versaloon/code/dongle/firmware/Projects/Versaloon/GCC/*.{hex,elf} /mnt
